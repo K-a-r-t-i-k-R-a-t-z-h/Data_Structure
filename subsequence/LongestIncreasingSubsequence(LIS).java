d6808de6a540bcafb0cfaa0c3858a05ac0956554
kutty <42936598+K-a-r-t-i-k-R-a-t-z-h@users.noreply.github.com>
2020-04-22 13:08:10 +0530

    Update LongestIncreasingSubsequence(LIS).java

--- subsequence/LongestIncreasingSubsequence(LIS).java
+++ subsequence/LongestIncreasingSubsequence(LIS).java
@@ -1,1 +1,51 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Lis
+{   
+static int tail[];
+    static void long1(int arr[],int n)
+    {
+     if(n==0)
+     return;
+     else
+     {
+     long1(arr,n-1);
 
+		     for(int i=0;i<n;i++)
+		     {
+	              if(arr[i]<arr[n])
+	              {
+	                if(tail[i]+1>tail[n])
+	                tail[n]=tail[i]+1;
+	              }
+		     }
+       
+     }
+    } 
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	tail=new int[n];
+	Arrays.fill(tail,1);
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++)
+	arr[i]=sc.nextInt();
+	long1(arr,n-1);
+	int max=Integer.MIN_VALUE;
+		for(int i=0;i<n;i++)
+		{
+          if(max<tail[i])
+          	max=tail[i];
+		}
+		System.out.println(max);
+	}
+}
+
+//https://media.geeksforgeeks.org/wp-content/cdn-uploads/Longest-Increasing-Subsequence.png
+
+/*output:
+
+8
+10 22 9 33 21 50 41 60
+5*/
