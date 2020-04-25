85e38170247b80e9f147493c0c0bef6a37335070
kutty <42936598+K-a-r-t-i-k-R-a-t-z-h@users.noreply.github.com>
2020-04-25 13:45:20 +0530

    Update and rename Bell.java to Bellman-Ford.java

--- Bellman-Ford.java
+++ Bellman-Ford.java
@@ -1,8 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 class Bell
 {   
 
-    static void bell(ArrayList<ArrayList<Integer>> arr,int fin,int n,int edge)
+    static void bell(ArrayList<ArrayList<Integer>> arr,int fin,int n,int edge)   //Bellman-ford Algorithm Implementation
     {
       int dis[]=new int[n];
       Arrays.fill(dis,Integer.MAX_VALUE);
@@ -13,14 +15,14 @@ class Bell
 
 		      for(int j=0;j<edge;j++)
 		      {
-              temp=new ArrayList<>();
-              temp=arr.get(j);
-              int src=temp.get(0);
-              int des=temp.get(1);
-              int wei=temp.get(2);
+			      temp=new ArrayList<>();
+			      temp=arr.get(j);
+			      int src=temp.get(0);
+			      int des=temp.get(1);
+			      int wei=temp.get(2);
 
-              if(dis[src]!=Integer.MAX_VALUE && wei+dis[src]<dis[des])
-              dis[des]=wei+dis[src];  
+			      if(dis[src]!=Integer.MAX_VALUE && wei+dis[src]<dis[des])
+			      dis[des]=wei+dis[src];  
 		      }
       }
     
@@ -52,3 +54,15 @@ class Bell
     bell(arr,fin,n+1,edge);
 	}
 }
+
+
+output:
+3
+3
+1 2 -10
+1 3 2
+3 2 4
+1
+1  0
+2  -10
+3  2
